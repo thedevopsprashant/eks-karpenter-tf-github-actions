767d40950ae49c34b8c7bbce315cf8714d865f33
Prashant <thedevopsprashant@gmail.com>
2026-03-17 21:51:19 +0530

    fixed backend

--- eks/backend.tf
+++ eks/backend.tf
@@ -2,15 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "~> 6.0.0"
     }
   }
   backend "s3" {
     bucket = "prashant-tfstates-bucket"
     region = "ap-south-1"
     key = "karpentereks/terraform.tfstate"
-    use_lockfile = true
-    encrypt = true
   }
 }
 
