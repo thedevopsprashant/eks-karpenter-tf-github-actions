81535659aeea99edafe235646779604154317279
Prashant <thedevopsprashant@gmail.com>
2026-03-31 21:58:38 +0530

    updated modeules

--- eks/main.tf
+++ eks/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "eks" {
-  source = "../module"
+  source = "../modules"
 
   env                   = var.env
   cluster-name          = "${local.env}-${local.org}-${var.cluster-name}"
